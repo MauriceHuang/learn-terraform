ab5f0b8a51136d9dede022ca793dea7a76134a38
mauricehuang <maurice.huang.pai.so@gmail.com>
2025-06-08 21:36:28 +0800

    added integration to the annoymizer lambda function

--- variables.tf
+++ variables.tf
@@ -21,5 +21,56 @@ variable "api_stage" {
   default     = "dev"
 }
 
+# 1. Create the API Gateway REST API
+resource "aws_api_gateway_rest_api" "api" {
+  name = "anonymizer-api"
+}
+
+# 2. Create a resource (e.g., /anonymize)
+resource "aws_api_gateway_resource" "anonymize" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "anonymize"
+}
+
+# 3. Create a POST method
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.anonymize.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+# 4. Integrate the method with Lambda
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.anonymize.id
+  http_method             = aws_api_gateway_method.post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.anonymizer.invoke_arn
+}
+
+# 5. Lambda permission for API Gateway
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.anonymizer.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+# 6. Deploy the API
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.lambda]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  stage_name    = "prod"
+}
+
 
 
